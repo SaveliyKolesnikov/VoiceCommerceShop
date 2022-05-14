e70b5031933696d7d9737817af53277b9a52f210
Savelii Kolesnikov <Savelii.Kolesnikov@covetrus.com>
2022-05-14 19:31:24 +0300

    Add project files.

---
+++ VoiceCommerceShop.DAL/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using VoiceCommerceShop.Domain;
+
+namespace VoiceCommerceShop.DAL;
+public class DataContext : DbContext
+{
+    protected readonly IConfiguration Configuration;
+
+    public DataContext(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+
+    }
+
+    public DbSet<Car> Cars { get; set; }
+}
---
+++ VoiceCommerceShop.Domain/Car.cs
@@ -0,0 +1,3 @@
+﻿namespace VoiceCommerceShop.Domain;
+
+public record Car(Guid CarKey, string Brand, string Color, double Price);
